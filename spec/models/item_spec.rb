c7ff12c5aaa8ae82677de1a892c15f8c7983d83d
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-10-03 11:41:17 +0200

    Add steps for unit test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :name}
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name}
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
+
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
@@ -25,5 +26,6 @@ RSpec.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
       with.library :rails 
+    end
   end
 end
