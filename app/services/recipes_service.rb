f47b9146a367332708fbcaf522ee65aad312c2cf
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-10-04 17:40:35 +0200

    Fix indentation in recipes_service.rb

--- app/services/recipes_service.rb
+++ app/services/recipes_service.rb
@@ -6,8 +6,6 @@ module RecipesService
         headers = {'application/json', 'id:35382': Rails.application.credentials.recipesearchapi [fc4dba13f609082f9b1488b63f415a5f] }
         url = "https://www.food2fork.com/api/search?key=fc4dba13f609082f9b1488b63f415a5f&q=ingredient"
         JSON.parse(RestClient.get(url, headers).body)
-
-
     end
 end
 
