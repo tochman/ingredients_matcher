bdfb2e71e94dd5cdaf8c61155e77b0bdf90238e9
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-10-04 18:59:44 +0200

    Modify api

--- app/services/recipes_service.rb
+++ app/services/recipes_service.rb
@@ -3,8 +3,8 @@ require 'rest-client'
 module RecipesService
 
     def self.get_recipes
-        headers = {'application/json', 'id:35382': Rails.application.credentials.recipesearchapi [fc4dba13f609082f9b1488b63f415a5f] }
-        url = "https://www.food2fork.com/api/search?key=fc4dba13f609082f9b1488b63f415a5f&q=ingredient"
+        headers = {'Content-Type': 'application/json', 'id:35382': Rails.application.credentials.recipeapi[fc4dba13f609082f9b1488b63f415a5f]}
+        url = "https://www.food2fork.com/api/search?key=#{fc4dba13f609082f9b1488b63f415a5f}&q=#{ingredient}"
         JSON.parse(RestClient.get(url, headers).body)
     end
 end
