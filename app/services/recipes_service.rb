fe3d6420ea327425b0251ccfc7b5a57b15bdfc8b
Noel Ryan <noelgerard.ryan@gmail.com>
2018-10-05 10:06:58 +0200

    Refactoring api url

--- app/services/recipes_service.rb
+++ app/services/recipes_service.rb
@@ -3,14 +3,14 @@ require 'rest-client'
 module RecipesService
 
     def self.get_recipes
-        headers = {'Content-Type': 'application/json', 'id:35382': Rails.application.credentials.recipeapi[fc4dba13f609082f9b1488b63f415a5f]}
-        url = "https://www.food2fork.com/api/search?key=#{fc4dba13f609082f9b1488b63f415a5f}&q=#{ingredient}"
+        headers = {'Content-Type': 'application/json', 'id:35382': Rails.application.credentials.api[:api_key]}
+        url = "https://www.food2fork.com/api/search?key=#{Rails.application.credentials.api[:api_key]}&q=#{ingredient}"
         JSON.parse(RestClient.get(url, headers).body)
     end
 end
 
 
-# fc4dba13f609082f9b1488b63f415a5f
+# 
 
-# https://www.food2fork.com/api/search?key=fc4dba13f609082f9b1488b63f415a5f&q=ingredient
+# https://www.food2fork.com/api/search?key=&q=ingredient
 # #{API-KEY}    #{ingredient}
