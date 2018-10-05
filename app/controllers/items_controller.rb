b57aa1132d223cd38ce39f65822249c9d95fde23
rhon <rhoneil.gabriel@me.com>
2018-10-05 16:16:10 +0200

    Title added in views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
-    @recipes = RecipesService.get_recipes{"image_url"}
+    @recipes = RecipesService.get_recipes["recipes"]
+    binding.pry
   end
 end
