2719fe3c7759170e38bf076e67ea5bac6f047588
Noel Ryan <noelgerard.ryan@gmail.com>
2018-10-05 10:53:27 +0200

    Generated controller and refactored code in the recipe services

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,5 @@
+class RecipesController < ApplicationController
+    def index
+        @recipes = Recipes.all
+    end
+end
--- app/services/recipes_service.rb
+++ app/services/recipes_service.rb
@@ -3,11 +3,8 @@ require 'rest-client'
 module RecipesService
 
     def self.get_recipes
-        headers = {'Content-Type': 'application/json', 'id:35382': Rails.application.credentials.api[:api_key]}
+        headers = {'Content-Type': 'application/json', : Rails.application.credentials.api[:api_key]}
         url = "https://www.food2fork.com/api/search?key=#{Rails.application.credentials.api[:api_key]}&q=#{ingredient}"
         JSON.parse(RestClient.get(url, headers).body)
     end
 end
-
-# https://www.food2fork.com/api/search?key=&q=ingredient
-# #{API-KEY}    #{ingredient}
