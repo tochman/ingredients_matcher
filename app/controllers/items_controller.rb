9a11ac9a112d2660b8013862a893059ff60f9c05
rhon <rhoneil.gabriel@me.com>
2018-10-07 08:01:57 +0200

    items check are showing but not connecting to search

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,12 @@ class ItemsController < ApplicationController
   
   def index
     @items = Item.all
-    binding.pry
     @recipes = RecipesService.get_recipes(nil)
 
   end
 
   def create
-    @recipes = RecipesService.get_recipes(params[:items])
+    @items = RecipesService.get_recipes(params[:items])
   end
 
 end
--- app/services/recipes_service.rb
+++ app/services/recipes_service.rb
@@ -3,9 +3,9 @@ require 'rest-client'
 module RecipesService
 
     def self.get_recipes(items)
-        ingredients = items.join(",")
+        seach_recipe = items.to_s
         headers = {'Content-Type': 'application/json'} 
-        url = "https://www.food2fork.com/api/search?key=#{Rails.application.credentials.api[:api_key]}&q=#{ingredients}"
+        url = "https://www.food2fork.com/api/search?key=#{Rails.application.credentials.api[:api_key]}&q=#{seach_recipe}"
         JSON.parse(RestClient.get(url, headers).body)["recipes"]
     end
 end
